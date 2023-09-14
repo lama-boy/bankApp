f2cba5d634e15934707ee5df58241a225958dfe3
lama-boy <ysl2884@gmail.com>
2023-09-14 16:14:00 +0900

    layout header, footer, 분리

---
+++ src/main/java/com/tencoding/bank/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.tencoding.bank.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String mainRedirect() {
+		// 뷰 리졸버 동작.
+		// prefix : /WEB-INF/view/
+		// sufix  : .jsp
+		return "layout/main";
+	}
+
+	// 주소 설계
+	// GET localhost:80/main-page
+	@GetMapping("main-page")
+	public String mainPage() {
+		// 뷰 리졸버 동작.
+		// prefix : /WEB-INF/view/
+		// sufix  : .jsp
+		return "layout/main";
+	}
+
+}
